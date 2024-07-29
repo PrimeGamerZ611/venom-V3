16e1e31c63298cf1747aa29f82301334004e13c6
PrimeGamerZ611 <pratikdeshmukh912@gmail.com>
2024-07-29 20:04:39 +0530

    m.py

--- m.py
+++ m.py
@@ -10,7 +10,7 @@ from keep_alive import keep_alive
 keep_alive()
 
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('TOKEM')
+bot = telebot.TeleBot('7439339572:AAEH-rzfHnufAqhUfTnoPxZFPCi7AcbSzek')
 # Admin user IDs
 admin_id = {"5188936165"}
 
